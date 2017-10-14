0d64d68466ed6b23a9581730940325bb093e3054
borellda <borella.daniel+github@gmail.com>
2017-10-14 14:45:29 +0200

    use cassandra docker image

--- src/main/java/horsediary/configuration/CassandraConfiguration.java
+++ src/main/java/horsediary/configuration/CassandraConfiguration.java
@@ -4,21 +4,25 @@ import horsediary.converter.DateConverter;
 import horsediary.converter.LocalDateConverter;
 import org.springframework.cassandra.config.CassandraCqlClusterFactoryBean;
 import org.springframework.cassandra.config.CassandraCqlSessionFactoryBean;
+import org.springframework.cassandra.core.keyspace.CreateKeyspaceSpecification;
+import org.springframework.cassandra.core.keyspace.DropKeyspaceSpecification;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.config.java.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.convert.CustomConversions;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
+@EnableCassandraRepositories(basePackages = "horsediary.model")
 public class CassandraConfiguration extends AbstractCassandraConfiguration {
-    @Override
-    public String[] getEntityBasePackages() {
-        return new String[] { "horsediary.model"};
-    }
+
+    public static final String KEYSPACE = "horsediaryspace";
 
     @Override
     public CustomConversions customConversions() {
@@ -36,6 +40,23 @@ public class CassandraConfiguration extends AbstractCassandraConfiguration {
 
     @Override
     protected String getKeyspaceName() {
-        return "horsediaryspace";
+        return KEYSPACE;
+    }
+
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.CREATE_IF_NOT_EXISTS;
+    }
+
+    @Override
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+        CreateKeyspaceSpecification specification = CreateKeyspaceSpecification.createKeyspace(KEYSPACE);
+
+        return Arrays.asList(specification);
+    }
+
+    @Override
+    protected List<DropKeyspaceSpecification> getKeyspaceDrops() {
+        return Arrays.asList(DropKeyspaceSpecification.dropKeyspace(KEYSPACE));
     }
 }
